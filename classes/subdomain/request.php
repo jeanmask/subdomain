a1118c7d74eccae6136994d2487c458a561ad952
Jean Oliveira <jean@webmais.net.br>
2011-03-12 12:33:19 -0300

    Reorganize classes

---
+++ classes/subdomain/request.php
@@ -0,0 +1,45 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Subdomain_Request extends Kohana_Request {
+
+	/**
+	 * @var  string  request Subdomain
+	 */
+	public static $subdomain;
+	
+	public static function factory($uri = TRUE, Cache $cache = NULL)	{
+		self::$subdomain = Request::detect_subdomain();
+		
+		return parent::factory($uri, $cache);
+	}
+	
+	public static function detect_subdomain($base = NULL, $host = NULL) {
+		if($base === NULL) {
+			$base = parse_url(Kohana::$base_url, PHP_URL_HOST);
+		}
+		
+		if($host === NULL) {
+			if( Kohana::$is_cli ) {
+				return FALSE;
+			}
+			
+			$host = $_SERVER['HTTP_HOST'];
+		}
+		
+		if(empty($base) || empty($host) || in_array($host, Route::$localhosts) || Valid::ip($host)) {
+			return FALSE;
+		}
+		
+		$sub_pos = ((int)strpos($host,$base))-1;
+		
+		if($sub_pos > 0) {
+			$subdomain = substr($host,0,$sub_pos);
+			
+			if(!empty($subdomain)) {
+				return $subdomain;
+			}
+		}
+		
+		return '';
+	}
+}
