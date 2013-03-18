10cb3d984bf67b5c38c408d7586b30b56ebffd8a
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2013-03-17 23:29:17 -0300

    fix code style

--- classes/subdomain/request.php
+++ classes/subdomain/request.php
@@ -5,7 +5,7 @@ class Subdomain_Request extends Kohana_Request {
 	/**
 	 * @var  string  request Subdomain
 	 */
-	public static $subdomain ;
+	public static $subdomain;
 			
 	public static function factory($uri = TRUE, $client_params = array(), $allow_external = TRUE, $injected_routes = array()){
 		self::$subdomain = Request::catch_subdomain() ;
@@ -14,32 +14,37 @@ class Subdomain_Request extends Kohana_Request {
 	}
 	
 	public static function catch_subdomain($base_url = NULL, $host = NULL) {
-		if($base_url === NULL) {
-			$base_url = parse_url(Kohana::$base_url, PHP_URL_HOST) ;
+		if($base_url === NULL) 
+		{
+			$base_url = parse_url(Kohana::$base_url, PHP_URL_HOST);
 		}
 		
-		if($host === NULL) {
-			if( php_sapi_name() == 'cli' && empty($_SERVER['REMOTE_ADDR']) ) {
-				return FALSE ;
+		if($host === NULL) 
+		{
+			if(php_sapi_name() == 'cli' AND empty($_SERVER['REMOTE_ADDR'])) 
+			{
+				return FALSE;
 			}
 			
-			$host = $_SERVER['HTTP_HOST'] ;
+			$host = $_SERVER['HTTP_HOST'];
 		}
 		
-		if(empty($base_url) || empty($host) || in_array($host, Route::$localhosts) || Valid::ip($host)) {
-			return FALSE ;
+		if(empty($base_url) OR empty($host) OR in_array($host, Route::$localhosts) OR Valid::ip($host)) 
+		{
+			return FALSE;
 		}
 		
-		$sub_pos = (int)strpos($host, $base_url) - 1 ;
+		$sub_pos = (int)strpos($host, $base_url) - 1;
 		
-		if($sub_pos > 0) {
-			$subdomain = substr($host,0,$sub_pos) ;
+		if($sub_pos > 0) 
+		{
+			$subdomain = substr($host, 0, $sub_pos);
 			
-			if( !empty($subdomain) ) {
+			if(! empty($subdomain)) {
 				return $subdomain ;
 			}
 		}
 		
-		return Route::SUBDOMAIN_EMPTY ;
+		return Route::SUBDOMAIN_EMPTY;
 	}
 }
--- classes/subdomain/route.php
+++ classes/subdomain/route.php
@@ -4,18 +4,18 @@ class Subdomain_Route extends Kohana_Route {
 
     const SUBDOMAIN_WILDCARD = '*', SUBDOMAIN_EMPTY = '' ;
 
-	public static $default_subdomains = array( self::SUBDOMAIN_EMPTY, 'www' ) ;
+	public static $default_subdomains = array(self::SUBDOMAIN_EMPTY, 'www');
 
 	/**
 	 * @var  string  route SUBDOMAIN
 	 */
-	protected $_subdomain ;
+	protected $_subdomain;
 	
 	public function __construct($uri = NULL, $regex = NULL) {
-		parent::__construct($uri, $regex) ;
+		parent::__construct($uri, $regex);
 		
         // Set default subdomains in this route rule
-		$this->_subdomain = self::$default_subdomains ;
+		$this->_subdomain = self::$default_subdomains;
 	}
 
 
@@ -32,19 +32,21 @@ class Subdomain_Route extends Kohana_Route {
      * @return Route
      */      
     public function subdomains(array $name) {
-        $this->_subdomain = $name ;
+        $this->_subdomain = $name;
 
-        return $this ;
+        return $this;
     }
 	
-	public function matches(Request $request){
-		$subdomain = (!isset($subdomain) || $subdomain === NULL) ? Request::$subdomain : $subdomain ;
+	public function matches(Request $request, $subdomain = NULL) {
+		$subdomain = (!isset($subdomain) OR $subdomain === NULL) ? Request::$subdomain : $subdomain;
 		
-		if($subdomain === FALSE) {
-			$subdomain = self::SUBDOMAIN_EMPTY ;
+		if($subdomain === FALSE) 
+		{
+			$subdomain = self::SUBDOMAIN_EMPTY;
 		}
 		
-		if( in_array(self::SUBDOMAIN_WILDCARD, $this->_subdomain) || in_array($subdomain, $this->_subdomain) ) {
+		if(in_array(self::SUBDOMAIN_WILDCARD, $this->_subdomain) OR in_array($subdomain, $this->_subdomain)) 
+		{
 			return parent::matches($request) ;
 		}
 		
